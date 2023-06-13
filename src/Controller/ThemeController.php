751b764451ea33ae3c26c1e84852728a0886f45e
Muhammadali Kavilumpara <muhammadali@Muhammadalis-MacBook-Pro-10.local>
2023-06-14 01:51:17 +0530

    first commit

---
+++ src/Controller/ThemeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ThemeController extends AbstractController
+{
+    #[Route('/theme/project', name: 'app_theme_project')]
+    public function index(): Response
+    {
+        return $this->render('theme/index.html.twig', [
+            'controller_name' => 'ThemeController',
+        ]);
+    }
+
+    #[Route('/theme/project/new', name: 'app_theme_project_add')]
+    public function new(): Response
+    {
+        return $this->render('theme/new.html.twig', [
+            'controller_name' => 'ThemeController',
+        ]);
+    }
+}
--- src/EventListener/CommandDispatchListener.php
+++ src/EventListener/CommandDispatchListener.php
@@ -2,7 +2,24 @@
 
 namespace App\EventListener;
 
-class MakeCrudCommandListener
-{
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
+#[AsEventListener]
+class CommandDispatchListener
+{
+    public function __invoke(ConsoleCommandEvent $commandEvent) :void
+    {
+        if (in_array($commandEvent->getCommand()->getName(), ['make:crud', 'make:registration-form']))
+        {
+            $io = new SymfonyStyle($commandEvent->getInput(), $commandEvent->getOutput());
+            $io->note('You are not authorized to use this feature!!!!!');
+
+            $io->success('You have a learnt a lot. Please practice');
+
+            exit;
+        }
+    }
 }
